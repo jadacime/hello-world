f6b40ada7ee1e0fa7ec338e822cff39be1dd1388
jadacime <javiercipagauta@gmail.com>
2016-07-28 18:14:42 -0500

    example of dowhile instructor

---
+++ DoWhile.java
@@ -0,0 +1,18 @@
+/**
+ * Created by javier on 20/06/16.
+ */
+public class DoWhile
+{
+    public static void main (String [] args)
+    {
+        int contador=1;
+        do
+        {
+           // System.out.println();
+            System.out.printf("%d ", contador);
+            ++contador;
+        }
+        while (contador <=10);
+        System.out.println();
+    }
+}
