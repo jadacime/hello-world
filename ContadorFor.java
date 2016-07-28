31ea4183a953fd42d79c1406224e44d57c04ea48
jadacime <javiercipagauta@gmail.com>
2016-07-28 18:15:11 -0500

    example of counter for

---
+++ ContadorFor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by javier on 4/06/16.
+ */
+public class ContadorFor
+{
+    public static void main (String [] args)
+    {
+        for(int i= 7; i<=77; i+=7)
+
+        System.out.printf("%d ", i);
+        System.out.println();
+       }
+}
