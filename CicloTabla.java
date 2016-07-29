4615c8c885f6c39f898ed2f1054151a53d66ee59
jadacime <javiercipagauta@gmail.com>
2016-07-29 10:47:22 -0500

    example  multiplication tables

---
+++ CicloTabla.java
@@ -0,0 +1,19 @@
+
+public class CicloTabla {
+
+	public static void main (String...javier)
+	{
+		int resultado;
+		for (int i=1; i<=10; i++)
+		{
+			System.out.println("");
+			System.out.println("tabla del "+i);
+			
+		for (int j=1;j<=10;j++)
+		{
+			resultado=i*j;
+			System.out.println(i+"*"+j+"="+resultado);
+		}
+	}
+	}
+}
